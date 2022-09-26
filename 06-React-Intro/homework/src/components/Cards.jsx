5dfba0942733cfd8a6e203112cfc6d6c1dc802c3
David <davidgabrielb@hotmail.com>
2022-09-26 13:49:10 -0300

    react

--- 06-React-Intro/homework/src/components/Cards.jsx
+++ 06-React-Intro/homework/src/components/Cards.jsx
@@ -3,5 +3,24 @@ import React from 'react';
 export default function Cards(props) {
   // acá va tu código
   // tip, podés usar un map
-  return <div>Cards Component</div>
+  let {cities} = props
+  return <div className='cards'>
+    
+    {cities.map((e, i) => {
+      return (
+        <div className='card' key={i}>
+          <button onClick={() => alert(e.name)} className='btnCards'>X</button>
+          <h4>{e.name}</h4>   
+          <div className='contenido'>
+            <div>Min</div>
+            <div>{e.main.temp_min}</div>
+            <div>Max</div>
+            <div>{e.main.temp_max}</div>
+            <img src={`http://openweathermap.org/img/wn/${e.weather[0].icon}@2x.png`} alt="img clima"/>
+          </div>
+          
+        </div>
+      )
+    })}
+  </div>
 };
