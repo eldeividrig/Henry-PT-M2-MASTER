6c73096061457825b81f57b54848d0a9ca6f38cf
eldeividrig <davidgabrielb@hotmail.com>
2022-10-13 12:57:56 -0300

    router

---
+++ 09-React-Routing/homework/src/components/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function About(){
+  return (
+    <div>
+        <h4>Barboza David</h4>
+        <h1>Ingeniero en Informática</h1>
+        <h1>Posadas Misiones Argentina</h1>
+    </div>
+  )
+}
+
+
